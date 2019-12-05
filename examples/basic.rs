3e67ea8d95649cb3fd864728e2763e0e99e194b4
Alec Deason <alec@tinycountry.com>
2019-12-05 15:40:35 -0800

    Basic example and a bit of cleanup

---
+++ examples/basic.rs
@@ -0,0 +1,13 @@
+use nalgebra as na;
+use rand::Rng;
+
+use quickhull::ConvexHull;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let size = 1000.0;
+    let points:Vec<_> = (0..5_000_000).map(|_| na::Point3::new(rng.gen_range(-size, size), rng.gen_range(-size, size), rng.gen_range(-size, size))).collect();
+
+    let hull = ConvexHull::from_points(&points).unwrap();
+    println!("Hull has {} triangles", hull.triangles.len());
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,14 @@ mod genmesh;
 
 type Point = na::Point3<f64>;
 #[derive(Clone)]
-pub struct Plane {
+pub struct Triangle {
     a: Point,
     b: Point,
     c: Point,
     normal: na::Vector3<f64>,
 }
 
-impl Plane {
+impl Triangle {
     fn new(a: Point, b: Point, c: Point) -> Self {
         let point1 = a - b;
         let point2 = b - c;
@@ -24,17 +24,17 @@ impl Plane {
 }
 
 pub struct ConvexHull {
-    triangles: Vec<Plane>,
+    pub triangles: Vec<Triangle>,
 }
 
 impl ConvexHull {
-    pub fn quickhull(points: &[Point]) -> Result<Self, ()> {
+    pub fn from_points(points: &[Point]) -> Result<Self, ()> {
         if points.len() < 4 {
             return Err(());
         };
         let plane = initial_triangle(points);
 
-        let triangles: Vec<Plane> = quick_hull(&plane, points, false)
+        let triangles: Vec<Triangle> = quick_hull(&plane, points, false)
             .into_iter()
             .chain(quick_hull(&plane, points, true))
             .collect();
@@ -48,13 +48,13 @@ impl ConvexHull {
     }
 }
 
-fn quick_hull(plane: &Plane, points: &[Point], invert: bool) -> Vec<Plane> {
+fn quick_hull(plane: &Triangle, points: &[Point], invert: bool) -> Vec<Triangle> {
     let mut max_dist = 0.0;
     let mut max_point = None;
     let live_points: Vec<_> = points
         .iter()
         .filter(|p| {
-            let mut d = signed_distance_to_plane(plane, p);
+            let mut d = plane.normal.dot(&(*p - &plane.a));
             if invert {
                 d *= -1.0;
             }
@@ -74,11 +74,11 @@ fn quick_hull(plane: &Plane, points: &[Point], invert: bool) -> Vec<Plane> {
     if let Some(max_point) = max_point {
         let max_point = *max_point;
         let possible_internal_points = [plane.a, plane.b, plane.c, max_point];
-        let mut plane_b = Plane::new(plane.a, plane.b, max_point);
+        let mut plane_b = Triangle::new(plane.a, plane.b, max_point);
         set_correct_normal(&mut plane_b, &possible_internal_points);
-        let mut plane_c = Plane::new(plane.b, plane.c, max_point);
+        let mut plane_c = Triangle::new(plane.b, plane.c, max_point);
         set_correct_normal(&mut plane_c, &possible_internal_points);
-        let mut plane_d = Plane::new(plane.c, plane.a, max_point);
+        let mut plane_d = Triangle::new(plane.c, plane.a, max_point);
         set_correct_normal(&mut plane_d, &possible_internal_points);
         quick_hull(&plane_b, &live_points, false)
             .into_iter()
@@ -90,7 +90,7 @@ fn quick_hull(plane: &Plane, points: &[Point], invert: bool) -> Vec<Plane> {
     }
 }
 
-fn initial_triangle(points: &[Point]) -> Plane {
+fn initial_triangle(points: &[Point]) -> Triangle {
     let mut x_max = &points[0];
     let mut x_min = &points[0];
     let mut y_max = &points[0];
@@ -115,13 +115,13 @@ fn initial_triangle(points: &[Point]) -> Plane {
         }
     }
 
-    let extreams = [*x_max, *y_max, *z_max, *x_min, *y_min, *z_min];
+    let extreams = [x_max, y_max, z_max, x_min, y_min, z_min];
 
     let mut max_dist = 0.0;
-    let mut line = [*x_max, *x_max];
+    let mut line = [x_max, x_max];
     for (i, a) in extreams.iter().enumerate() {
         for b in &extreams[i + 1..] {
-            let d = na::distance(a, b);
+            let d = na::distance(*a, *b);
             if d > max_dist {
                 max_dist = d;
                 line = [*a, *b];
@@ -130,13 +130,13 @@ fn initial_triangle(points: &[Point]) -> Plane {
     }
     let third_point = *points
         .iter()
-        .max_by_key(|p| NotNan::new(distance_to_line(&line, p).unwrap_or(0.0)).unwrap())
+        .max_by_key(|p| NotNan::new(distance_to_line(&line, p)).unwrap())
         .unwrap();
 
-    Plane::new(line[0], line[1], third_point)
+    Triangle::new(*line[0], *line[1], third_point)
 }
 
-fn set_correct_normal(plane: &mut Plane, points: &[Point]) {
+fn set_correct_normal(plane: &mut Triangle, points: &[Point]) {
     for p in points.iter() {
         let d = plane.normal.dot(&(p - plane.a));
         if d > 10.0f64.powf(-10.0) {
@@ -146,18 +146,14 @@ fn set_correct_normal(plane: &mut Plane, points: &[Point]) {
     }
 }
 
-fn distance_to_line(line: &[Point; 2], point: &Point) -> Result<f64, ()> {
+fn distance_to_line(line: &[&Point; 2], point: &Point) -> f64 {
     let vec1 = point - line[0];
     let vec2 = point - line[1];
     let vec4 = vec1.cross(&vec2);
 
     if vec2.magnitude() <= 0.0 {
-        Err(())
+        0.0
     } else {
-        Ok(vec4.magnitude() / vec2.magnitude())
+        vec4.magnitude() / vec2.magnitude()
     }
-}
-
-fn signed_distance_to_plane(plane: &Plane, point: &Point) -> f64 {
-    plane.normal.dot(&(point - plane.a))
 }
